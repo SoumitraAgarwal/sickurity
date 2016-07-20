a0717ac175a5d72b86fc93e5ad3b3a7604446443
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-20 19:42:12 +0530

    Add debug commit

--- thresh.py
+++ thresh.py
@@ -17,15 +17,15 @@ while True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         if frame_num == 1:
             firstFrame = gray
-        #     # if frame_num == 10:
-        #     # 	frame_num=0
-        #     gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        #     frameDelta = cv2.absdiff(firstFrame, gray)
-        #     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-        #     thresh = cv2.dilate(thresh, None, iterations=2)
-        #     cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        #     for c in cnts:
-        #     # if the contour is too small, ignore it
+            if frame_num == 10:
+            	frame_num=0
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+            frameDelta = cv2.absdiff(firstFrame, gray)
+            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            # cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            # for c in cnts:
+            # if the contour is too small, ignore it
         #         if cv2.contourArea(c) < 10:
         #             continue
         #         (x, y, w, h) = cv2.boundingRect(c)
