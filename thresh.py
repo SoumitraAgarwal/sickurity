17a67e3334c4089a01a1be1367981b0c2794d527
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-20 19:40:08 +0530

    Add debug commit

--- thresh.py
+++ thresh.py
@@ -15,25 +15,25 @@ while True:
     if ret:
         frame_num = frame_num+1
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if frame_num == 1:
-            firstFrame = gray
-            # if frame_num == 10:
-            # 	frame_num=0
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-            frameDelta = cv2.absdiff(firstFrame, gray)
-            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-            for c in cnts:
-            # if the contour is too small, ignore it
-                if cv2.contourArea(c) < 10:
-                    continue
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                if cv2.contourArea(c) > 200000:
-                    frame_num = 0
-                    scene_num = scene_num+1
-                    print(scene_num)
+        # if frame_num == 1:
+        #     firstFrame = gray
+        #     # if frame_num == 10:
+        #     # 	frame_num=0
+        #     gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        #     frameDelta = cv2.absdiff(firstFrame, gray)
+        #     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        #     thresh = cv2.dilate(thresh, None, iterations=2)
+        #     cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        #     for c in cnts:
+        #     # if the contour is too small, ignore it
+        #         if cv2.contourArea(c) < 10:
+        #             continue
+        #         (x, y, w, h) = cv2.boundingRect(c)
+        #         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        #         if cv2.contourArea(c) > 200000:
+        #             frame_num = 0
+        #             scene_num = scene_num+1
+        #             print(scene_num)
 
                 # Display the resulting frame
 		# cv2.imshow('Video', thresh)
